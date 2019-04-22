50655a2471e1b98461ac36abfe9e3cfb58bb7e3e
Dubravka_Trampa <dudatramp@gmail.com>
2019-04-22 17:57:02 +0200

    save

--- LotoRotoProjekat/TestStranica.aspx.cs
+++ LotoRotoProjekat/TestStranica.aspx.cs
@@ -17,13 +17,13 @@ namespace LotoRotoProjekat
 
         protected void buttonZavrsiKolo_Click(object sender, EventArgs e)
         {
-            string naredbaZavrsiKolo = "INSERT INTO Fondovi SELECT SUM(iznos) FROM Transakcije WHERE tip_transakcije = 'tiket' AND datum between '2019-04-20' AND '2019-04-23';";
+           /* string naredbaZavrsiKolo = "INSERT INTO Fondovi SELECT SUM(iznos) FROM Transakcije WHERE tip_transakcije = 'tiket' AND datum between '2019-04-20' AND '2019-04-23';";
             SqlConnection conn = konekcija.Connect();
             SqlCommand komandaNaciIdRacuna = new SqlCommand(naredbaZavrsiKolo, conn);
             SqlCommand komandaUnesiNoviRacun = new SqlCommand(naredbaZavrsiKolo, conn);
             conn.Open();
             komandaUnesiNoviRacun.ExecuteNonQuery();
-            int fkRacuniId = Int32.Parse(komandaNaciIdRacuna.ExecuteScalar().ToString());
+            int fkRacuniId = Int32.Parse(komandaNaciIdRacuna.ExecuteScalar().ToString());*/
         }
     }
 }

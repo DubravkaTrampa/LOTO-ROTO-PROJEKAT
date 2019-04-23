42202f427fc137a332a5385f90a41573aff05684
Dubravka_Trampa <dudatramp@gmail.com>
2019-04-24 01:24:25 +0200

    .

--- LotoRotoProjekat/TestStranica.aspx.cs
+++ LotoRotoProjekat/TestStranica.aspx.cs
@@ -11,6 +11,17 @@ namespace LotoRotoProjekat
     public partial class WebForm1 : System.Web.UI.Page
     {
         List<Dobitnik> dobitnici = new List<Dobitnik>();
+
+        int cenaJednogTiketa = 100;
+        // Pojedinacne uplatne vrednosti zavisno od dobitka
+        double fondZaDobitak4Pogotka = 0;
+        double fondZaDobitak5Pogotka = 0;
+        double fondZaDobitak6Pogotka = 0;
+        double fondZaDobitak7Pogotka = 0;
+        double fondZaHumanitarneSvrhe = 0;
+        double fondZaNaplatuAdministratoru = 0;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -43,28 +54,17 @@ namespace LotoRotoProjekat
            // int fkRacuniId = Int32.Parse(komandaNaciIdRacuna.ExecuteScalar().ToString());
         }
 
-        int cenaJednogTiketa = 100;
-        // Pojedinacne uplatne vrednosti zavisno od dobitka
-        double fondZaDobitak4Pogotka = 0;
-        double fondZaDobitak5Pogotka = 0;
-        double fondZaDobitak6Pogotka = 0;
-        double fondZaDobitak7Pogotka = 0;
-        double fondZaHumanitarneSvrhe = 0;
-        double fondZaNaplatuAdministratoru = 0;
-
-        private class Dobitnik
+        protected void buttonZapocniKolo_Click(object sender, EventArgs e)
         {
-            public string username;
-            public string vrstaPogotka;
-            public int idRacuna;
-            public int idTiketa;
 
-            public Dobitnik(string vrstaPogotka, int idRacuna)
-            {
-                this.vrstaPogotka = vrstaPogotka;
-                this.idRacuna = idRacuna;
-            }
+            bool izvucenaSedmica = true;
+            UcitajDobitnike();
+            IzracunajFondove(VratiNagradniFondIzBaze(), 4, izvucenaSedmica);
+            IzvrsiNaplateIzFonda();
+            AzurirajFond(izvucenaSedmica);
         }
+
+     
         public void UcitajDobitnike()
         {
             //UPITI ZA PRONALAZENJE BROJA RACUNA, I PREKO BROJA RACUNA NACI USERNAME SVIH DOBITNIKA. TEST PODACI
@@ -86,48 +86,53 @@ namespace LotoRotoProjekat
             }
         }
 
-        public void NaplatiKorisniku(int idRacuna,
-                                     double novac,
-                                     string usernameKorisnika)
+        public void IzracunajFondove(double ukupanFond,
+                                    int brojDobitnika4Kombinacije,
+                                    bool izvucenaSedmica)
         {
-            string tipTransakcije = "'nagrada'";
-            string datum ="'" + DateTime.Now.ToString("yyyy-MM-dd") + "'";
+            //Izvaja se fond za cetiri dobitaka(zamena) od ukupnog fonda, pre racunanja fondova za vece dobitke
+            ukupanFond -= fondZaDobitak4Pogotka = brojDobitnika4Kombinacije * cenaJednogTiketa;
+            fondZaDobitak5Pogotka = VratiProcenat(ukupanFond, 10d);
+            fondZaDobitak6Pogotka = VratiProcenat(ukupanFond, 30d);
+            fondZaDobitak7Pogotka = VratiProcenat(ukupanFond, 60d);
 
-            string naredbaNapraviTransakcijuKorisniku = "INSERT INTO Transakcije " +
-                "(iznos,datum,fk_racuni_id,tip_transakcije) " +
-                "VALUES (" +
-                 novac + "," + 
-                 datum + "," +
-                 idRacuna + "," +
-                 tipTransakcije + ");";
+            fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak5Pogotka, 20d);
+            fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak5Pogotka, 2d);
+            fondZaDobitak5Pogotka -= VratiProcenat(fondZaDobitak5Pogotka, 22d);
 
-            new SqlCommand(naredbaNapraviTransakcijuKorisniku, konekcija.Connect());
+            fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak6Pogotka, 20d);
+            fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak6Pogotka, 2d);
+            fondZaDobitak6Pogotka -= VratiProcenat(fondZaDobitak6Pogotka, 22d);
+
+            if (izvucenaSedmica)
+            {
+                fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak7Pogotka, 20d);
+                fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak7Pogotka, 2d);
+                fondZaDobitak7Pogotka -= VratiProcenat(fondZaDobitak7Pogotka, 22d);
+            }
         }
 
-        public void NaplatiOrganizacijama(int idRacunaHumanitarne,int idRacunaVlasnika, int novac)
+        public double VratiNagradniFondIzBaze()
         {
-            string tipTransakcije = "'humanitarna'";
-            string datum = "'" + DateTime.Now.ToString("yyyy-MM-dd") + "'";
-
-            string naredbaNapraviTransakcijuKorisniku = "INSERT INTO Transakcije " +
-                "(iznos,datum,fk_racuni_id,tip_transakcije) " +
-                "VALUES (" +
-                 novac + "," +
-                 datum + "," +
-                 idRacunaHumanitarne + "," +
-                 tipTransakcije + ");";
-
-            new SqlCommand(naredbaNapraviTransakcijuKorisniku, konekcija.Connect());
+            string naredbaVratiPoslednjuVrednostFonda = "SELECT stanje_na_fondu " +
+                "FROM Fondovi " +
+                "WHERE [pk_fondovi_id] = (SELECT MAX(pk_fondovi_id) FROM Fondovi)";
+            SqlConnection conn = konekcija.Connect();
+            conn.Open();
+            SqlCommand komandaVratiStanjeNaFondu = new SqlCommand(naredbaVratiPoslednjuVrednostFonda, conn);
+            int ukupanFond = Int32.Parse(komandaVratiStanjeNaFondu.ExecuteScalar().ToString());
+            conn.Close();
+            return Convert.ToDouble(ukupanFond);
         }
+
 
         public void IzvrsiNaplateIzFonda()
-        {   //IZVUCI IZ BAZE ODREDJENIM UPITOM KAKO BI MOGLI DA RASPOREDIMO NAGRADE
+        {   //IZVUCI IZ BAZE - BROJEVE DOBITNIKA ODREDJENIM UPITOM KAKO BI MOGLI DA RASPOREDIMO NAGRADE
             double pojedinacniDobitakZa4Pogotka = fondZaDobitak4Pogotka / 4d;
             double pojedinacniDobitakZa5Pogotka = fondZaDobitak5Pogotka / 3d;
             double pojedinacniDobitakZa6Pogotka = fondZaDobitak6Pogotka / 2d;
             double pojedinacniDobitakZa7Pogotka = fondZaDobitak7Pogotka / 1d;
             string datum = "'" + DateTime.Now.ToString("yyyy-MM-dd") + "'";
-            //IZVUCI IZ TABELE DOBITNIKA BROJ DOBITNIKA
             foreach (Dobitnik trenutni in dobitnici)
             {
                 if(trenutni.vrstaPogotka == "4")
@@ -169,57 +174,40 @@ namespace LotoRotoProjekat
             conn.Close();
         }
 
-        public double VratiNagradniFond()
+        public void AzurirajFond(bool izvucenaSedmica)
         {
-            string naredbaVratiPoslednjuVrednostFonda = "SELECT stanje_na_fondu " +
-                "FROM Fondovi " +
-                "WHERE [pk_fondovi_id] = (SELECT MAX(pk_fondovi_id) FROM Fondovi)";
-            SqlConnection conn= konekcija.Connect();
-            conn.Open();
-            SqlCommand komandaVratiStanjeNaFondu = new SqlCommand(naredbaVratiPoslednjuVrednostFonda, conn);
-            int ukupanFond = Int32.Parse(komandaVratiStanjeNaFondu.ExecuteScalar().ToString());
-            conn.Close();
-            return Convert.ToDouble(ukupanFond);
-        }
-
-        public void IzracunajFondove(double ukupanFond,
-                                     int brojDobitnika4Kombinacije,
-                                     bool izvucenaSedmica)
-        {
-            //Izvaja se fond za cetiri dobitaka(zamena) od ukupnog fonda, pre racunanja fondova za vece dobitke
-            ukupanFond -= fondZaDobitak4Pogotka = brojDobitnika4Kombinacije * cenaJednogTiketa;
-            fondZaDobitak5Pogotka = VratiProcenat(ukupanFond, 10d);
-            fondZaDobitak6Pogotka = VratiProcenat(ukupanFond, 30d);
-            fondZaDobitak7Pogotka = VratiProcenat(ukupanFond, 60d);
-
-
-            fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak5Pogotka, 20d);
-            fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak5Pogotka, 2d);
-            fondZaDobitak5Pogotka -= VratiProcenat(fondZaDobitak5Pogotka, 22d);
-
-            fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak6Pogotka, 20d);
-            fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak6Pogotka, 2d);
-            fondZaDobitak6Pogotka -= VratiProcenat(fondZaDobitak6Pogotka, 22d);
-
-
-
+            string naredbaAzuriraj = "";
             if (izvucenaSedmica)
             {
-                fondZaHumanitarneSvrhe += VratiProcenat(fondZaDobitak7Pogotka, 20d);
-                fondZaNaplatuAdministratoru += VratiProcenat(fondZaDobitak7Pogotka, 2d);
-                fondZaDobitak7Pogotka -= VratiProcenat(fondZaDobitak7Pogotka, 22d);
+              naredbaAzuriraj = "INSERT INTO Fondovi (stanje_na_fondu) VALUES (0)";
+            }
+            else
+            {
+                naredbaAzuriraj = "INSERT INTO Fondovi (stanje_na_fondu) VALUES (" + fondZaDobitak7Pogotka + "); ";
+            }
+            SqlConnection conn = konekcija.Connect();
+            conn.Open();
+            new SqlCommand(naredbaAzuriraj, conn).ExecuteNonQuery();
+            conn.Close();
+        }
+        private class Dobitnik
+        {
+            public string username;
+            public string vrstaPogotka;
+            public int idRacuna;
+            public int idTiketa;
+
+            public Dobitnik(string vrstaPogotka, int idRacuna)
+            {
+                this.vrstaPogotka = vrstaPogotka;
+                this.idRacuna = idRacuna;
             }
         }
+
         public double VratiProcenat(double vrednost, double procenat)
         {
             return vrednost / (100d / procenat);
         }
 
-        protected void buttonZapocniKolo_Click(object sender, EventArgs e)
-        {
-            UcitajDobitnike();
-            IzracunajFondove(VratiNagradniFond(),4,true);
-            IzvrsiNaplateIzFonda();
-        }
     }
 }
